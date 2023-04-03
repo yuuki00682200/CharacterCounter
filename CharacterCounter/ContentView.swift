8235e6110ac3ed97fb210b05204103c2acf7be87
Yuki Takatsu <yukitakatsu6468@gmail.com>
2023-04-03 22:15:00 +0900

    ライトモードとダークモードの切り替えを実装

--- CharacterCounter/ContentView.swift
+++ CharacterCounter/ContentView.swift
@@ -10,23 +10,61 @@ import SwiftUI
 struct ContentView: View {
     @State private var inputText: String = ""
     @State private var characterCount: Int = 0
+    @State private var includeSpaces: Bool = true
+    @State private var isDarkMode: Bool = false
     
     var body: some View {
-        VStack(spacing: 20) {
+        let backgroundColor = isDarkMode ? Color.black : Color.white
+        let textColor = isDarkMode ? Color.white : Color.black
+        
+        return VStack(spacing: 20) {
             Text("Character Counter")
-                .font(.largeTitle)
+                .font(.system(size: 40, weight: .bold))
+                .foregroundColor(textColor)
+                .shadow(color: .gray, radius: 2, x: 2, y: 2)
             
             TextEditor(text: $inputText)
                 .border(Color.gray, width: 1)
                 .padding()
+                .shadow(color: .gray, radius: 2, x: 2, y: 2)
                 .onChange(of: inputText) { newValue in
-                    characterCount = newValue.count
+                    characterCount = countCharacters(text: newValue, includeSpaces: includeSpaces)
                 }
             
+            Toggle(isOn: $includeSpaces) {
+                Text("Include spaces")
+                    .font(.system(size: 18, weight: .bold))
+                    .foregroundColor(textColor)
+                    .shadow(color: .gray, radius: 1, x: 1, y: 1)
+            }
+            .padding()
+            .onChange(of: includeSpaces) { _ in
+                characterCount = countCharacters(text: inputText, includeSpaces: includeSpaces)
+            }
+            
+            Toggle(isOn: $isDarkMode) {
+                Text("Dark Mode")
+                    .font(.system(size: 18, weight: .bold))
+                    .foregroundColor(textColor)
+                    .shadow(color: .gray, radius: 1, x: 1, y: 1)
+            }
+            .padding()
+            
             Text("Character count: \(characterCount)")
-                .font(.headline)
+                .font(.system(size: 18, weight: .bold))
+                .foregroundColor(textColor)
+                .shadow(color: .gray, radius: 1, x: 1, y: 1)
         }
         .padding()
+        .background(backgroundColor.ignoresSafeArea())
+    }
+    
+    func countCharacters(text: String, includeSpaces: Bool) -> Int {
+        if includeSpaces {
+            return text.count
+        } else {
+            return text.filter { !$0.isWhitespace }.count
+        }
     }
 }
 
